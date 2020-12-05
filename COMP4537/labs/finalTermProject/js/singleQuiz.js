bf3871a2eae7e7c0e3bed98a88d261105f6e4b9e
Hevano <evanohirabe@gmail.com>
2020-12-05 12:36:00 -0800

    Some progress on single quiz

--- COMP4537/labs/finalTermProject/js/singleQuiz.js
+++ COMP4537/labs/finalTermProject/js/singleQuiz.js
@@ -1,113 +1,131 @@
+const host = "https://agile-tundra-39359.herokuapp.com/api/v1/"
+const quiz = null;
 
-  (function(){
-    function buildQuiz(){
-      // variable to store the HTML output
-      const output = [];
-  
-      // for each question...
-      myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-  
-          // variable to store the list of possible answers
-          const answers = [];
-  
-          // and for each available answer...
-          for(letter in currentQuestion.answers){
-  
-            // ...add an HTML radio button
-            answers.push(
-              `<label>
-                <input type="radio" name="question${questionNumber}" value="${letter}">
-                ${letter} :
-                ${currentQuestion.answers[letter]}
-              </label>`
-            );
-          }
-  
-          // add this question and its answers to the output
-          output.push(
-            `<div class="question"> ${currentQuestion.question} </div>
-            <div class="answers"> ${answers.join('')} </div>`
-          );
-        }
-      );
-  
-      quizContainer.innerHTML = output.join('');
-    }
-  
-    // function showResults(){
-  
-    //   // gather answer containers from our quiz
-    //   const answerContainers = quizContainer.querySelectorAll('.answers');
-  
-    //   // keep track of user's answers
-    //   let numCorrect = 0;
-  
-    //   // for each question...
-    //   myQuestions.forEach( (currentQuestion, questionNumber) => {
-  
-    //     // find selected answer
-    //     const answerContainer = answerContainers[questionNumber];
-    //     const selector = `input[name=question${questionNumber}]:checked`;
-    //     const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-  
-    //     // if answer is correct
-    //     if(userAnswer === currentQuestion.correctAnswer){
-    //       // add to the number of correct answers
-    //       numCorrect++;
-  
-    //       // color the answers green
-    //       answerContainers[questionNumber].style.color = 'lightgreen';
-    //     }
-    //     // if answer is wrong or blank
-    //     else{
-    //       // color the answers red
-    //       answerContainers[questionNumber].style.color = 'red';
-    //     }
-    //   });
-  
-    //   // show number of correct answers out of total
-    //   resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-    // }
-  
-    const quizContainer = document.getElementById('quiz');
-    const submitButton = document.getElementById('submit');
-    const myQuestions = [
-      {
-        question: "Who invented JavaScript?",
-        answers: {
-          a: "Hello",
-          b: "Sheryl Sandberg",
-          c: "Brendan Eich",
-          d: "Hello"
-        }
-      },
-      {
-        question: "Which one of these is a JavaScript package manager?",
-        answers: {
-          a: "Node.js",
-          b: "TypeScript",
-          c: "npm",
-          d: "hello"
-        }
-      },
-      {
-        question: "Which tool can you use to ensure code quality?",
-        answers: {
-          a: "Angular",
-          b: "jQuery",
-          c: "RequireJS",
-          d: "ESLint"
-        }
+buildQuiz = ()=>{
+  // variable to store the HTML output
+  const output = [];
+
+  // for each question...
+  myQuestions.forEach(
+    (currentQuestion, questionNumber) => {
+
+      // variable to store the list of possible answers
+      const answers = [];
+
+      // and for each available answer...
+      for(letter in JSON.parse(currentQuestion.answers)){
+
+        // ...add an HTML radio button
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
       }
-    ];
 
+      // add this question and its answers to the output
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join('')} </div>`
+      );
+    }
+  );
 
-  
-    // Kick things off
+  quizContainer.innerHTML = output.join('');
+}
+
+showResults = ()=>{
+
+  // gather answer containers from our quiz
+  const answerContainers = quizContainer.querySelectorAll('.answers');
+
+  // keep track of user's answers
+  let numCorrect = 0;
+
+  // for each question...
+  myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+    // find selected answer
+    const answerContainer = answerContainers[questionNumber];
+    const selector = `input[name=question${questionNumber}]:checked`;
+    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+    // if answer is correct
+    if(userAnswer === currentQuestion.correctAnswer){
+      // add to the number of correct answers
+      numCorrect++;
+
+      // color the answers green
+      answerContainers[questionNumber].style.color = 'lightgreen';
+    }
+    // if answer is wrong or blank
+    else{
+      // color the answers red
+      answerContainers[questionNumber].style.color = 'red';
+    }
+  });
+
+  // show number of correct answers out of total
+  resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+}
+
+populate = ()=>{
+  const quizContainer = document.getElementById('quiz');
+  const submitButton = $('#submit');
+  submitButton.onClick(showResults());
+
+  fetch(host + "/quiz/" + quiz.id + "/questions", requestOptions)
+  .then((response) => {
+    if (response.status == 409) throw response.json()
+    return response.json()
+
+  })
+  .then((result) => {
+    const myQuestions = result;
     buildQuiz();
-  
-  })();
+  })
+  .catch(err => err.then(msg => alert(msg.message)));
+}
+
+
+// const myQuestions = [
+//   {
+//     question: "Who invented JavaScript?",
+//     answers: {
+//       a: "Hello",
+//       b: "Sheryl Sandberg",
+//       c: "Brendan Eich",
+//       d: "Hello"
+//     }
+//   },
+//   {
+//     question: "Which one of these is a JavaScript package manager?",
+//     answers: {
+//       a: "Node.js",
+//       b: "TypeScript",
+//       c: "npm",
+//       d: "hello"
+//     }
+//   },
+//   {
+//     question: "Which tool can you use to ensure code quality?",
+//     answers: {
+//       a: "Angular",
+//       b: "jQuery",
+//       c: "RequireJS",
+//       d: "ESLint"
+//     }
+//   }
+// ];
+
+
+
+// Kick things off
+
+
+
 
   var ct = 1;
   function new_link(length)
